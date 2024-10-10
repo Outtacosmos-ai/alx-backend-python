64da7b53be1c67004be064e7a8c358eb9f5505c5
Outtacosmos-ai <messghir4@gmail.com>
2024-10-10 14:11:56 +0100

    done ✅

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python3
+"""Module for zoom_array function."""
 from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
-    """Zooms in on an array by repeating its elements."""
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Create a new list with each element of the input tuple repeated.
+
+    Args:
+        lst: A tuple of elements
+        factor: The number of times to repeat each element
+
+    Returns:
+        A list with each element from the input repeated 'factor' times
+    """
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-# Convert the array to a tuple before passing it to zoom_array
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
+
 zoom_3x = zoom_array(array, 3)
-
-
-if __name__ == "__main__":
-    print(zoom_array.__annotations__)
