627878b55f6aaa9c4215d63eec212296337e86b7
Outtacosmos-ai <messghir4@gmail.com>
2024-10-15 23:56:42 +0100

    done ✅

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,8 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    Coroutine that yields a random number between 0 and 10 every second, 10 times.
+    Coroutine that yields a random number between 0 and 10 every second,
+    10 times.
 
     Yields:
         float: A random number between 0 and 10.
